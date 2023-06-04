6a01fcb19d5d2b312b5fc432fb81b4e7644fed54
Anibal <alandro@gmail.com>
2023-06-03 21:15:46 -0300

    Initial commit

---
+++ src/main/java/ar/com/codoacodo/controllers/UserController.java
@@ -0,0 +1,96 @@
+package ar.com.codoacodo.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.codoacodo.domain.User;
+import ar.com.codoacodo.dto.UserRequestDTO;
+import ar.com.codoacodo.dto.UserResponseDTO;
+import ar.com.codoacodo.services.UserService;
+import lombok.RequiredArgsConstructor;
+
+//http://localhost:8080/user
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+	//necesito el service
+	private final UserService userService;
+	
+	//GET
+	@GetMapping("/{id}")	
+	public ResponseEntity<User> m1(
+			@PathVariable("id") Long id
+			) {
+		
+		User user = this.userService.buscarUser(id);
+		
+		//http status code=200
+		return ResponseEntity.ok(user);
+	}
+	
+	@GetMapping()	
+	public ResponseEntity<List<User>> findAll() {
+		
+		List<User> user = this.userService.buscarTodos();
+		
+		//http status code=200
+		return ResponseEntity.ok(user);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<UserResponseDTO> createUser(
+			@RequestBody UserRequestDTO request
+		)  {
+		
+		//verifico si existe
+		User user = this.userService.buscarUserPorUsername(request.getUsername());
+		if(user != null) {
+			UserResponseDTO response = UserResponseDTO.builder()
+					.username(user.getUsername())
+					.build();
+				
+			return ResponseEntity.ok(response);
+		}
+				
+		//validacion!!!
+		User newUser = User.builder()
+				.username(request.getUsername())
+				.password(request.getPassword())
+				.build();
+				
+		this.userService.crearUser(newUser);
+		
+		UserResponseDTO response = UserResponseDTO.builder()
+			.username(newUser.getUsername())
+			.build();
+		
+		return ResponseEntity.ok(response);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> borrarUser(
+			@PathVariable("id") Long id
+			){
+		this.userService.eliminarUser(id);
+		return ResponseEntity.ok().build();
+	}
+	
+	/*user/1
+	{
+	  alias:"nuevoalias"
+		id:2
+	}
+	*/
+	
+}
